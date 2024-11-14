02bf53962c411f28d136a56e4acf5010cf7c44a0
pbartashov <p.bartashov@icloud.com>
2024-11-14 22:56:02 +1030

    Move URL creation to a helper method

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -38,7 +38,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
 
     func test_getFromURL_performsGETRequestWithURL() {
-        let url = URL(string: "http://any-url.com")!
+        let url = anyURL()
         let exp = expectation(description: "Wait for request")
 
         URLProtocolStub.observeRequest { request in
@@ -53,13 +53,12 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
 
     func test_getFromURL_failsOnRequestError() {
-        let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
 
         let exp = expectation(description: "Wait for comletion")
 
-        makeSUT().get(from: url) { result in
+        makeSUT().get(from: anyURL()) { result in
             switch result {
                 case let .failure(receivedError as NSError):
                     XCTAssertEqual(error.domain, receivedError.domain)
@@ -84,6 +83,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
         trackForMemoryLeaks(sut)
 
         return sut
+    }
+
+    private func anyURL() -> URL {
+        URL(string: "http://any-url.com")!
     }
 
     private class URLProtocolStub: URLProtocol {
