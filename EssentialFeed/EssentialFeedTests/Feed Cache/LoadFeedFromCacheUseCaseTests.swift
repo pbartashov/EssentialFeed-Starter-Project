2b5f0b95ba895e6caaa281b42af89df4586a9f71
pbartashov <p.bartashov@icloud.com>
2024-11-29 22:58:15 +1030

    LocalFeedLoader does not message store upon creation

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -1,115 +1,20 @@
 //
-//  CacheFeedUseCaseTests.swift
+//  LoadFeedFromCacheUseCaseTests.swift
 //  EssentialFeedTests
 //
-//  Created by Pavel Bartashov on 25/11/2024.
+//  Created by Pavel Bartashov on 29/11/2024.
 //
 
 import XCTest
 import EssentialFeed
 
-final class CacheFeedUseCaseTests: XCTestCase {
+final class LoadFeedFromCacheUseCaseTests: XCTestCase {
 
     func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
 
         XCTAssertEqual(store.receivedMessages, [])
     }
-
-    func test_saveRequestsCacheDeletion() {
-        let (sut, store) = makeSUT()
-
-        sut.save(uniqueImageFeed().models) { _ in }
-
-        XCTAssertEqual(store.receivedMessages, [.deleteCacheFeed])
-    }
-
-    func test_save_doesNotRequestCacheInsertionOnDeletionError() {
-        let (sut, store) = makeSUT()
-        let deletionError = anyNSError()
-
-        sut.save(uniqueImageFeed().models) { _ in }
-        store.completeDeletion(with: deletionError)
-
-        XCTAssertEqual(store.receivedMessages, [.deleteCacheFeed])
-    }
-
-    func test_save_requestsInsertionWithTimeStampOnSuccessfulDeletion() {
-        let timeStamp = Date()
-        let (sut, store) = makeSUT(currentDate: { timeStamp })
-        let feed = uniqueImageFeed()
-
-        sut.save(feed.models) { _ in }
-        store.completeDeletionSuccessfully()
-
-        XCTAssertEqual(store.receivedMessages, [.deleteCacheFeed, .insert(feed.local, timeStamp)])
-    }
-
-    func test_save_failsOnDeletionError() {
-        let timeStamp = Date()
-        let (sut, store) = makeSUT(currentDate: { timeStamp })
-        let deletionError = anyNSError()
-
-        expect(sut, toCompleteWithError: deletionError) {
-            store.completeDeletion(with: deletionError)
-        }
-    }
-
-    func test_save_failsOnInsertionError() {
-        let timeStamp = Date()
-        let (sut, store) = makeSUT(currentDate: { timeStamp })
-        let insertionError = anyNSError()
-
-        expect(sut, toCompleteWithError: insertionError) {
-            store.completeDeletionSuccessfully()
-            store.completeInsertion(with: insertionError)
-        }
-    }
-
-    func test_save_succeedsOnSuccessfulCacheInsertion() {
-        let timeStamp = Date()
-        let (sut, store) = makeSUT(currentDate: { timeStamp })
-
-        expect(sut, toCompleteWithError: nil) {
-            store.completeDeletionSuccessfully()
-            store.completeInsertionSuccessfully()
-        }
-    }
-
-    func test_save_doesNotDeliverDeletionErrorAfterSUTInstanceHasBeenDeallocated() {
-        let store = FeedStoreSpy()
-        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
-
-        var receivedResults = [LocalFeedLoader.SaveResult?]()
-
-        sut?.save([uniqueImage()]) { error in
-            receivedResults.append(error)
-        }
-
-        sut = nil
-        store.completeDeletion(with: anyNSError())
-
-        XCTAssertTrue(receivedResults.isEmpty)
-    }
-
-    func test_save_doesNotDeliverInsertionErrorAfterSUTInstanceHasBeenDeallocated() {
-        let store = FeedStoreSpy()
-        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
-
-        var receivedResults = [LocalFeedLoader.SaveResult?]()
-
-        sut?.save([uniqueImage()]) { error in
-            receivedResults.append(error)
-        }
-
-        store.completeDeletionSuccessfully()
-        sut = nil
-        store.completeInsertion(with: anyNSError())
-
-        XCTAssertTrue(receivedResults.isEmpty)
-    }
-
-    // MARK: - Helpers
 
     private func makeSUT(
         currentDate: @escaping () -> Date = Date.init,
