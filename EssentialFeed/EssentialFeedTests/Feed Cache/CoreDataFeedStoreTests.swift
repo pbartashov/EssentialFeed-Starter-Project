372985ccd261191e2bb57d524051c0e867efb155
pbartashov <p.bartashov@icloud.com>
2024-12-11 23:09:36 +1030

    Add empty CoreData feed store specs

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,83 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Pavel Bartashov on 11/12/2024.
+//
+
+import XCTest
+import EssentialFeed
+
+
+//class CoreDataFeedStore {
+//
+//    init(storeURL: URL, bundle: Bundle) throws {
+//
+//    }
+//}
+
+
+final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+
+    }
+    
+    func test_retrieve_hasNoEffectsOnEmptyCache() {
+
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+
+    }
+    func test_insert_deliversNoErrorsOnEmptyCache() {
+
+    }
+    
+    func test_insert_deliversNoErrorsOnNonEmptyCache() {
+
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+
+    }
+    
+    func test_storeSideEffects_runSerially() {
+
+    }
+    
+
+
+
+    // MARK: - Helpers
+
+//    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
+//        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+//        let storeURL = URL(fileURLWithPath: "/dev/null")
+//        let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+//        trackForMemoryLeaks(sut, file: file, line: line)
+//
+//        return sut
+//    }
+
+
+}
