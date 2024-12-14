590747e3800fa2d12665bcd91ce4209840a6a76a
pbartashov <p.bartashov@icloud.com>
2024-12-14 16:38:07 +1030

    Add 'EssentialFeedCacheIntegrationTests' target to separate the potentially-slower cache integration tests from the fast uinit/isolation tests

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Pavel Bartashov on 14/12/2024.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+
+}
