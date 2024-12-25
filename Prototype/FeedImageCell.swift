9b9ab4f85df34df314b96035e5e4456943ad563e
pbartashov <p.bartashov@icloud.com>
2024-12-26 00:02:30 +1030

    Add 'FeedImageCell' with storyboard connections

---
+++ Prototype/FeedImageCell.swift
@@ -0,0 +1,8 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Pavel Bartashov on 25/12/2024.
+//
+
+import Foundation
---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Pavel Bartashov on 25/12/2024.
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var desctriptionLabel: UILabel!
+}
