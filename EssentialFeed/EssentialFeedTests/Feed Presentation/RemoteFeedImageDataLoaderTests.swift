457c5c3712b04689ae80c022b7c3e4a6ff506cb3
pbartashov <p.bartashov@icloud.com>
2025-02-04 21:57:52 +1030

    'RemoteFeedImageDataLoader' requests data from URL on 'loadImageData'

--- EssentialFeed/EssentialFeedTests/Feed Presentation/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/RemoteFeedImageDataLoaderTests.swift
@@ -6,9 +6,19 @@
 //
 
 import XCTest
+import EssentialFeed
 
 final class RemoteFeedImageDataLoader {
 
+    private let client: HTTPClient
+
+    init(client: HTTPClient) {
+        self.client = client
+    }
+
+    func loadImageData(from url: URL, completion: @escaping (Any) -> Void) {
+        client.get(from: url) { _ in }
+    }
 }
 
 final class RemoteFeedImageDataLoaderTests: XCTestCase {
@@ -16,15 +26,27 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
     func test_init_doesNotPerformAnyURLRequest() {
         let (_, client) = makeSUT()
 
-        XCTAssertTrue(client.requestedURLS.isEmpty)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
 
+    func test_loadImageDataFromURL_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.loadImageData(from: url) { _ in }
+
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+
+    // MARK: - Helpers
+
     private func makeSUT(
+        url: URL = anyURL(),
         file: StaticString = #filePath,
         line: UInt = #line
     ) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
-        let client = HTTPClientSpy  ()
-        let sut = RemoteFeedImageDataLoader()
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
 
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
@@ -32,7 +54,11 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         return (sut, client)
     }
 
-    private class HTTPClientSpy {
-        var requestedURLS = [URL]()
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURLs = [URL]()
+
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+            requestedURLs.append(url)
+        }
    }
 }
