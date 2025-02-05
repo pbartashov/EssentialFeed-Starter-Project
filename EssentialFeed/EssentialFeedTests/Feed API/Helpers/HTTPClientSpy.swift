fffc938d99c8719969109086315c171e34dbf1d1
pbartashov <p.bartashov@icloud.com>
2025-02-05 16:48:02 +1030

    Extract duplicate 'HTTPClientSpy' helper into a shared scope.

---
+++ EssentialFeed/EssentialFeedTests/Feed API/Helpers/HTTPClientSpy.swift
@@ -0,0 +1,46 @@
+//
+//  HTTPClientSpy.swift
+//  EssentialFeed
+//
+//  Created by Pavel Bartashov on 5/2/2025.
+//
+
+import Foundation
+import EssentialFeed
+
+final class HTTPClientSpy: HTTPClient {
+    private struct Task: HTTPClientTask {
+        let callback: () -> Void
+        func cancel() { callback() }
+    }
+
+    private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+
+    var requestedURLs: [URL] {
+        return messages.map { $0.url }
+    }
+
+    var cancelledURLs = [URL]()
+
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        messages.append((url, completion))
+
+        return Task { [weak self] in
+            self?.cancelledURLs.append(url)
+        }
+    }
+
+    func complete(with error: Error, at index: Int = 0) {
+        messages[index].completion(.failure(error))
+    }
+
+    func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+        let response = HTTPURLResponse(
+            url: requestedURLs[index],
+            statusCode: code,
+            httpVersion: nil,
+            headerFields: nil
+        )!
+        messages[index].completion(.success((data, response)))
+    }
+}
