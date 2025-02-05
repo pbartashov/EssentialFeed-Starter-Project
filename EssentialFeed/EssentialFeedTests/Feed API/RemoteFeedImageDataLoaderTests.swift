d752da0452a293ebfef41b3565ce168de4908059
pbartashov <p.bartashov@icloud.com>
2025-02-05 16:09:54 +1030

    Cancelling LoadImageDataURLTask cancels client URL request

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -13,7 +13,7 @@ final class RemoteFeedImageDataLoader {
         let wrapped: HTTPClientTask
 
         func cancel() {
-//            wrapped.cancel()
+            wrapped.cancel()
         }
     }
 
@@ -125,7 +125,18 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
 
         XCTAssertTrue(capturedResults.isEmpty)
     }
-    
+
+    func test_cancelLoadImageDataURLTask_cancelsClientURLRequest() {
+        let (sut, client) = makeSUT()
+        let url = URL(string: "https://a-given-url.com")!
+
+        let task = sut.loadImageData(from: url) { _ in }
+        XCTAssertTrue(client.cancelledURLs.isEmpty, "Expected no cancelled URL request until task is cancelled")
+
+        task.cancel()
+        XCTAssertEqual(client.cancelledURLs, [url], "Expected cancelled URL request after task is cancelled")
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(
@@ -176,7 +187,8 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
 
     private class HTTPClientSpy: HTTPClient {
         private struct Task: HTTPClientTask {
-            func cancel() {}
+            let callback: () -> Void
+            func cancel() { callback() }
         }
 
         private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
@@ -185,10 +197,14 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
             return messages.map { $0.url }
         }
 
+        var cancelledURLs = [URL]()
+
         func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
             messages.append((url, completion))
 
-            return Task()
+            return Task { [weak self] in
+                self?.cancelledURLs.append(url)
+            }
         }
 
         func complete(with error: Error, at index: Int = 0) {
