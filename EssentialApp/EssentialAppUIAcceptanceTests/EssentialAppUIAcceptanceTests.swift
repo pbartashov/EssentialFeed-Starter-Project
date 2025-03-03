c3bf2994e7081bccfa89ca3aef5389c992851ece
pbartashov <p.bartashov@icloud.com>
2025-03-03 22:05:10 +1030

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Pavel Bartashov on 3/3/2025.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+
+   
+    
+}
