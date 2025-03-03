d2ac294541f9cb5934cf1ba2c7deb14b39104b7d
pbartashov <p.bartashov@icloud.com>
2025-03-03 22:33:51 +1030

    Reset cache in every test run to make sure tests run in a clean state and don't influence the result of oyher tests.

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -14,7 +14,7 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
 
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
-
+        app.launchArguments = ["-reset"]
         app.launch()
 
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
@@ -26,6 +26,7 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
 
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
         let onlineApp = XCUIApplication()
+        onlineApp.launchArguments = ["-reset"]
         onlineApp.launch()
 
         let offlineApp = XCUIApplication()
