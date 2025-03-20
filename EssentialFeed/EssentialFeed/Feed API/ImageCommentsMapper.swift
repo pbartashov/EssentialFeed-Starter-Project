4eb62a45ff386af76cdf1e2e62c7ab4e302b6d28
pbartashov <p.bartashov@icloud.com>
2025-03-20 19:15:44 +1030

    Implement ImageComment mapping

--- EssentialFeed/EssentialFeed/Feed API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/ImageCommentsMapper.swift
@@ -8,20 +8,38 @@
 
 import Foundation
 
- final class ImageCommentsMapper {
-     private struct Root: Decodable {
-         let items: [RemoteFeedItem]
-     }
+final class ImageCommentsMapper {
+    private struct Root: Decodable {
+        private let items: [Item]
 
-     static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
-         guard isOK(response), let root = try? JSONDecoder().decode(Root.self, from: data) else {
-             throw RemoteImageCommentsLoader.Error.invalidData
-         }
+        private struct Item: Decodable {
+            let id: UUID
+            let message: String
+            let created_at: Date
+            let author: Author
+        }
 
-         return root.items
-     }
+        private struct Author: Decodable {
+            let username: String
+        }
 
-     private static func isOK(_ response: HTTPURLResponse) -> Bool {
-         (200...299).contains(response.statusCode)
-     }
- }
+        var comments: [ImageComment] {
+            items.map { ImageComment(id: $0.id, message: $0.message, createdAt: $0.created_at, username: $0.author.username) }
+        }
+    }
+
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        guard isOK(response), let root = try? decoder.decode(Root.self, from: data) else {
+            throw RemoteImageCommentsLoader.Error.invalidData
+        }
+
+        return root.comments
+    }
+
+    private static func isOK(_ response: HTTPURLResponse) -> Bool {
+        (200...299).contains(response.statusCode)
+    }
+}
