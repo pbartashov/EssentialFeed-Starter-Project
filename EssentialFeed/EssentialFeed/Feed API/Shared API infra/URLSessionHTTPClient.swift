9ae427e11b7655bc17bf2bcdcd98bdb7df1528fe
pbartashov <p.bartashov@icloud.com>
2025-03-20 19:22:36 +1030

    Move 'HTTPClient' protocol and implementation to standalone folders representing modules.

--- EssentialFeed/EssentialFeed/Feed API/Shared API infra/URLSessionHTTPClient.swift
+++ EssentialFeed/EssentialFeed/Feed API/Shared API infra/URLSessionHTTPClient.swift
@@ -9,21 +9,21 @@ import Foundation
 
 public final class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
-
+    
     public init (session: URLSession) {
         self.session = session
     }
-
+    
     private struct UnexpectedValuesRepresentationError: Error { }
-
+    
     private struct URLSessionTaskWrapper: HTTPClientTask {
         let wrapped: URLSessionTask
-
+        
         func cancel() {
             wrapped.cancel()
         }
     }
-
+    
     public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> any HTTPClientTask {
         let task =  session.dataTask(with: url) { data, response, error in
             completion(Result {
@@ -37,7 +37,7 @@ public final class URLSessionHTTPClient: HTTPClient {
             })
         }
         task.resume()
-
+        
         return URLSessionTaskWrapper(wrapped: task)
     }
 }
