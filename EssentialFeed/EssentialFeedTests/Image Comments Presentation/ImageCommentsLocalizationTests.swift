989d944f88cf551654c880f1ff39761c68040448
pbartashov <p.bartashov@icloud.com>
2025-03-31 22:07:41 +1030

    Localize image comments title

---
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCommentsLocalizationTests.swift
+//  EssentialFeed
+//
+//  Created by Pavel Bartashov on 31/3/2025.
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsLocalizationTests: XCTestCase {
+
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+
+}
