b39152aa888e36a39240e8aa43d56cdf54abb8bf
pbartashov <p.bartashov@icloud.com>
2025-04-24 11:10:47 +0930

    Use applySnapshotUsingReloadData on iOS 15+ to maintain the same data source behavior we have on iOS 14 and before.

--- EssentialFeed/EssentialFeediOS/Feed UI/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Shared UI/Controllers/ListViewController.swift
@@ -72,7 +72,12 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
         var snapshot = NSDiffableDataSourceSnapshot<Int, CellController>()
         snapshot.appendSections([0])
         snapshot.appendItems(cellControllers, toSection: 0)
-        dataSource.apply(snapshot)
+
+        if #available(iOS 15.0, *) {
+            dataSource.applySnapshotUsingReloadData(snapshot)
+        } else {
+            dataSource.apply(snapshot)
+        }
     }
 
     public override func viewIsAppearing(_ animated: Bool) {
